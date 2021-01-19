f1411b3a32989e17adbf2ba9ba50ce6ead15eed9
Demonbane18 <butterncreamz@gmail.com>
2021-01-19 12:20:51 +0800

    install firebase

--- real-time-chat/src/App.vue
+++ real-time-chat/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- install firebase -->
+  <!-- add rules no-unused-vars on package.json -->
+  <!-- delete hello world and styles -->
+  <!-- add heading Hello World -->
+  <!-- add setup -->
+<h1>Hello World</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ setup() {
+   return {}
+ }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
