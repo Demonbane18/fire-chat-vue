ab5694d36743229054cb9a464727669f8c357f2a
Demonbane18 <butterncreamz@gmail.com>
2021-01-19 12:34:41 +0800

    added keys and style

--- real-time-chat/src/db.js
+++ real-time-chat/src/db.js
@@ -3,6 +3,15 @@ import "firebase/database";
 
 const config = {
     //API KEYS
+    //add api keys
+    apiKey: "AIzaSyCE_pkSAn5-vsB7YOH2LSuMIGxzA4w93ps",
+    authDomain: "firechat-vue-1716a.firebaseapp.com",
+    databaseURL: "https://firechat-vue-1716a-default-rtdb.firebaseio.com",
+    projectId: "firechat-vue-1716a",
+    storageBucket: "firechat-vue-1716a.appspot.com",
+    messagingSenderId: "161844580258",
+    appId: "1:161844580258:web:bbc3d5a24660e906e66cfb",
+    measurementId: "G-5SDBHJ7KZX"
 }
 
 const db = firebase.initializeApp(config);
